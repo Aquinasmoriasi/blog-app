11460588b03135fbcd0153026fbd08a0a6942a02
unknown <aquinasmoriasi@gmail.com>
2022-10-27 11:52:51 +0300

    correct author

--- db/schema.rb
+++ db/schema.rb
@@ -19,22 +19,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_080432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.index ["address"], name: "index_comments_on_address"
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.index ["address"], name: "index_likes_on_address"
+    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,9 +45,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_080432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.index ["address"], name: "index_posts_on_address"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,8 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_080432) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
