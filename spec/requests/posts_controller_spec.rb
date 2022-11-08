af239efe8a07a62049447ed3b26c5e9adc0719ba
unknown <aquinasmoriasi@gmail.com>
2022-11-08 22:41:52 +0300

    correct controller spec errors

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-    before(:example) { get '/users/1/posts' }
+    before(:each) { 
+      @user = User.create(
+        name: 'Tom',
+        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        bio: 'Rapper from Detroit'
+      )
+      get "/users/#{@user.id}/posts"
+    }
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
@@ -11,7 +18,15 @@ RSpec.describe 'Posts', type: :request do
     end
   end
   describe 'GET #show' do
-    before(:example) { get '/users/1/posts/1' }
+    before(:each) do
+      @user = User.create(
+        name: 'Tom',
+        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        bio: 'Rapper from Detroit'
+      )
+      @post = Post.create(author: @user, title: 'Hello', text: 'This is one of the first posts')
+      get "/users/#{@user.id}/posts/#{@post.id}"
+    end
     it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
@@ -19,7 +34,6 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template('posts/show')
     end
     it 'shows if the specific user template is rendered correctly' do
-      get '/users/1/posts/1'
       expect(response.body).to include('back to all users')
     end
   end
