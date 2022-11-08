327b7e1f72639374a0e48f98e49d10cb3b76d664
unknown <aquinasmoriasi@gmail.com>
2022-11-08 07:54:15 +0300

    add cascade on dependents

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :author, class_name: 'User'
   after_save :update_post_counter
   validates :title, presence: true, length: { maximum: 250 }
