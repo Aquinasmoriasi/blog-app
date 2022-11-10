4fdbd449a3e35df99edb0a871925a51d294390c6
unknown <aquinasmoriasi@gmail.com>
2022-11-10 13:47:43 +0300

    add login and logout method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
-class ApplicationController < ActionController::Base 
-  before_action :authenticate_user! protect_from_forgery with: :exception 
-  before_action :update_allowed_parameters, if: :devise_controller? 
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
 
-  protected 
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-  def update_allowed_parameters 
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)} devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :bio, :email, :password, :current_password)} 
-  end 
-end 
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :bio, :email, :password, :current_password)}
+  end
+end
